901b4f3b7c339544e9ca5228990ee8ce3c0a3044
Aron Galvan <arongalvan03@gmail.com>
2022-05-23 19:58:47 -0700

    commit tree da646cf6e2d1760a98224a4b47fdeafe0d8a022e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const countDown = (number) => {
+    for (let i = number; i >= 0; i--)
+    {
+        console.log(i);
+    }
+}
+
+const writeCards = (array) => {
+    let messagesArray = [];
+    for (let i = 0; i < array.length; i++)
+    {
+        messagesArray[i] = `Thank you, ${array[i]}, for the wonderful surprise gift!`;
+    }
+    return messagesArray;
+}
